4cbe88d4caee75074c39821837fe53d308aa2a2a
TobiasWilhelmsson <143700814+TobiasWilhelmsson@users.noreply.github.com>
2024-02-21 13:54:04 +0100

    New calendar module

--- src/components/CalendarBox.vue
+++ src/components/CalendarBox.vue
@@ -1,72 +1,92 @@
 <template>
-  <Qalendar :events="events" :config="config" />
+  <div>
+    <bryntum-calendar-project-model
+      ref="project"
+      v-bind="projectConfig"
+      :resources="resources"
+      :events="events"
+    />
+    <bryntum-calendar ref="calendar" v-bind="calendarConfig" />
+  </div>
 </template>
 
 <script>
-import { Qalendar } from 'qalendar'
+import { ref, reactive, onMounted } from 'vue'
+
+import { BryntumCalendarProjectModel, BryntumCalendar } from '@bryntum/calendar-vue-3'
+
+import { useCalendarConfig, useProjectConfig } from '@/AppConfig'
+import '@bryntum/calendar/locales/calendar.locale.SvSE.js'
+import { LocaleManager, LocaleHelper } from '@bryntum/calendar/calendar.module.js'
+
+LocaleManager.locale = LocaleHelper.locales.SvSE
 
 export default {
+  name: 'App',
+
   components: {
-    Qalendar
+    BryntumCalendarProjectModel,
+    BryntumCalendar
   },
 
-  data() {
-    return {
-      events: [
-        {
-          title: 'Möte Sprint 1',
-          with: 'Team 4',
-          time: { start: '2024-02-20 11:15', end: '2024-02-20 12:00' },
-          colorScheme: 'meetings',
-          isEditable: true,
-          id: 'de471c78cb5c',
-          description: 'Vi ska gå igenom Sprint 1 planering med Richard.'
-        }
-      ],
-      config: {
-        week: {
-          // Takes the value 'sunday' or 'monday'
-          // However, if startsOn is set to 'sunday' and nDays to 5, the week displayed will be Monday - Friday
-          startsOn: 'monday',
-          // Takes the values 5 or 7.
-          nDays: 7,
-          // Scroll to a certain hour on mounting a week. Takes any value from 0 to 23.
-          // This option is not compatible with the 'dayBoundaries'-option, and will simply be ignored if custom day boundaries are set.
-          scrollToHour: 5
-        },
-        month: {
-          // Hide leading and trailing dates in the month view (defaults to true when not set)
-          showTrailingAndLeadingDates: false
-        },
-        // Takes any valid locale that the browser understands. However, not all locales have been thorougly tested in Qalendar
-        // If no locale is set, the preferred browser locale will be used
-        locale: 'sv-SE',
-        style: {
-          // When adding a custom font, please also set the fallback(s) yourself
-          fontFamily: 'Nunito, sans-serif',
-          colorSchemes: {
-            meetings: {
-              color: '#fff',
-              backgroundColor: '#131313'
-            },
-            chill: {
-              color: '#fff',
-              backgroundColor: '#ff4081'
-            }
-          }
-        },
-        // if not set, the mode defaults to 'week'. The three available options are 'month', 'week' and 'day'
-        // Please note, that only day and month modes are available for the calendar in mobile-sized wrappers (~700px wide or less, depending on your root font-size)
-        defaultMode: 'week',
-        // The silent flag can be added, to disable the development warnings. This will also bring a slight performance boost
-        isSilent: true,
-        showCurrentTime: true // Display a line indicating the current time
+  setup() {
+    const calendar = ref(null)
+    const project = ref(null)
+
+    const calendarConfig = reactive(useCalendarConfig())
+    const projectConfig = reactive(useProjectConfig())
+
+    const resources = ref(null)
+    const events = ref(null)
+
+    resources.value = [
+      {
+        id: 1,
+        name: 'Per',
+        eventColor: 'green'
+      },
+      {
+        id: 2,
+        name: 'Anna',
+        eventColor: 'blue'
+      },
+      {
+        id: 3,
+        name: 'Röda dagar',
+        eventColor: 'red'
+      },
+      {
+        id: 4,
+        name: 'Namnsdagar',
+        eventColor: 'black'
       }
+    ]
+    events.value = [
+      {
+        id: 1,
+        name: 'Möte',
+        startDate: '2024-02-21T11:00:00',
+        endDate: '2024-02-21T12:00:00',
+        resourceId: 1
+      }
+    ]
+
+    onMounted(() => {
+      calendar.value.instance.value.project = project.value.instance.value
+    })
+
+    return {
+      project,
+      calendar,
+      projectConfig,
+      calendarConfig,
+      resources,
+      events
     }
   }
 }
 </script>
 
-<style>
-@import 'qalendar/dist/style.css';
+<style lang="scss">
+@import 'src/App.scss';
 </style>
