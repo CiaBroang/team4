c1c991a8ad91110ec6aa4f981c966458114f3ffe
TobiasWilhelmsson <143700814+TobiasWilhelmsson@users.noreply.github.com>
2024-03-07 14:03:47 +0100

    About and contact page

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,47 +1,50 @@
-
-
 <template>
-  <v-layout>
-  <v-footer class="custom-footer">
-    <v-row justify="center" no-gutters>
-      <v-col cols="12" class="d-flex justify-center">
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          color="white"
-          variant="text"
-          class="mx-2"
-          rounded="xl"
-        >
-          {{ link }}
-        </v-btn>
-      </v-col>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Pocket planner</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
-</v-layout>
-</template>
+    <v-layout>
+      <v-footer class="custom-footer">
+        <v-row justify="center" no-gutters>
+          <v-col cols="12" class="d-flex justify-center">
+            <a
+              v-for="link in links"
+              :key="link.text"
+              :href="link.url"
+              class="mx-2"
+            >
+              <v-btn
+                color="white"
+                variant="text"
+                rounded="xl"
+              >
+                {{ link.text }}
+              </v-btn>
+            </a>
+          </v-col>
+          <v-col class="text-center mt-4" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Pocket planner</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    </v-layout>
+  </template>
 
-<script>
-export default {
-  data: () => ({
-    links: [
-      'About Us',
-      'Contact',
-      'Jobs'
-    ],
-  }),
-}
-</script>
+  <script>
+  export default {
+    data() {
+      return {
+        links: [
+          { text: 'About Us', url: '/about' },
+          { text: 'Contact', url: '#' },
+          { text: 'Jobs', url: '#' }
+        ],
+      };
+    },
+  };
+  </script>
 
-<style>
-.custom-footer {
-  background-color: #ff851b !important ;
-  position: absolute;
-  bottom: 0;
-  height: 75px !important ;
-}
-
-</style>
+  <style>
+  .custom-footer {
+    background-color: #ff851b !important;
+    position: absolute;
+    bottom: 0;
+    height: 75px !important;
+  }
+  </style>
